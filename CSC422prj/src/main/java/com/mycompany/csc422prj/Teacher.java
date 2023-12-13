eb36e668f6994afef51463a0576b99fbbdfa02f3
KristinGraceBetzler <152664199+KristinGraceBetzler@users.noreply.github.com>
2023-12-13 08:32:34 -0600

    Update Teacher.java
    added chooseWeapon method

--- CSC422prj/src/main/java/com/mycompany/csc422prj/Teacher.java
+++ CSC422prj/src/main/java/com/mycompany/csc422prj/Teacher.java
@@ -21,6 +21,33 @@ public class Teacher extends Survivor
         this.dead = false;
     }
 
+    public void chooseWeapon()
+     {
+        Weapon[] availableWeapons =
+        {
+            new RangedWeapon("Shotgun", 30, 0.7, 8, "Blast"),
+            new RangedWeapon("Submachine gun", 25, 0.8, 20, "Spray"),
+            new RangedWeapon("Assault Rifle", 35, 0.75, 30, "Fire"),
+            new RangedWeapon("Pistol", 20, 0.9, 1, "Shoot"),
+            new MeleeWeapon("Axe", 40, 0.6, "Chop"),
+            new MeleeWeapon("Crowbar", 25, 0.2, "Smash"),
+            new MeleeWeapon("Frying Pan", 15, 0.85, "Hit")
+        };
+
+        Random random = new Random();
+        Weapon chosenWeapon = availableWeapons[random.nextInt(availableWeapons.length)];
+
+        System.out.println(chosenWeapon.getName() + " has chosen the weapon: " + chosenWeapon.getName());
+        System.out.println("Damage: " + chosenWeapon.getAttack());
+        System.out.println("Accuracy: " + chosenWeapon.getAccuracy());
+        if (chosenWeapon instanceof RangedWeapon rangedWeapon)
+        {
+            System.out.println("Num Projectiles: " + rangedWeapon.getNumProjectiles());
+        }
+        System.out.println("Kill Action: " + chosenWeapon.getKillAction());
+    }
+
+
     public void attack(Zombie target) 
     {
         Zombie zombie = (Zombie) target;
